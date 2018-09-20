022fd9fbcea6b9a2334a438d51e1bbf8f25a88ff
tgrassi <tommasograssi@gmail.com>
2018-09-20 13:51:23 +0200

    PY: handles freqences, plot types

--- utils.py
+++ utils.py
@@ -4,13 +4,14 @@ import numpy as np
 class Qabs_utils:
     import matplotlib.pyplot as plt
 
-
     # ******************
     def __init__(self):
         self.data = dict()
         self.data_ref = dict()
         self.data_q = dict()
         self.inputs = ["wlen", "real_eps1", "im_eps", "real_m1", "im_m"]
+
+        self.constants = {"clight": 2.99792458e10}
 
     # *******************************
     # load refractive index data from a refractive index file named fname
@@ -32,7 +33,7 @@ class Qabs_utils:
             labs = self.inputs
 
         # init data dictionary
-        data = {lab:[] for lab in labs}
+        data = {lab: [] for lab in labs}
         # loop on file to read
         for row in open(fname, "rb"):
             srow = row.strip().replace("\t", " ")
@@ -40,7 +41,8 @@ class Qabs_utils:
                 continue
             arow = [float(x) for x in srow.split(" ") if x != ""]
             if len(labs) != len(arow):
-                sys.exit("ERROR: file " + fname + " has more columns than the list provided in labs argument!")
+                sys.exit("ERROR: file " + fname +
+                         " has more columns than the list provided in labs argument!")
             for ii, lab in enumerate(labs):
                 data[lab].append(arow[ii])
 
@@ -49,7 +51,10 @@ class Qabs_utils:
             irev = -1
         else:
             irev = 1
-        data = {lab: np.array(x[::irev]) for lab,x in data.iteritems()}
+        data = {lab: np.array(x[::irev]) for lab, x in data.iteritems()}
+
+        # convert to Hz
+        data["freq"] = self.constants["clight"] / (data["wlen"] * 1e-4)
 
         # find real_m from real_m1 if needed
         if "real_m1" in data:
@@ -74,6 +79,11 @@ class Qabs_utils:
             e2 = data["im_eps"]
             data["real_m"] = e2 / 2. / data["im_m"]
 
+        # compute im_m1 from im_m if missing
+        if "real_m1" not in data:
+            print "NOTE: real_m1 computed from real_m"
+            data["real_m1"] = data["real_m"] - 1e0
+
         print "Loading from " + fname + " done!"
 
         self.data = data
@@ -90,7 +100,7 @@ class Qabs_utils:
     # load qabs data for comparison
     def load_qref(self, fname):
         labs = ["wlen", "qabs_ref", "qsca_ref", "gcos_ref"]
-        data_qref = {lab:[] for lab in labs}
+        data_qref = {lab: [] for lab in labs}
         for row in open(fname, "rb"):
             srow = row.strip()
             if row.startswith("#") or srow == "":
@@ -98,7 +108,7 @@ class Qabs_utils:
             arow = [float(x) for x in srow.split(" ") if x != ""]
             for ii, lab in enumerate(labs):
                 data_qref[lab].append(arow[ii])
-        self.data_ref = {lab: np.array(x) for lab,x in data_qref.iteritems()}
+        self.data_ref = {lab: np.array(x) for lab, x in data_qref.iteritems()}
 
     # ************************
     # compute qabs and qsca
@@ -109,7 +119,7 @@ class Qabs_utils:
         # local copy of data to work with
         data = self.data
 
-        data_q = {"qabs": [], "qsca": [], "qbak":[]}
+        data_q = {"qabs": [], "qsca": [], "qbak": []}
         # loop on wavelengths to call bhmie
         for ii, wlen in enumerate(data["wlen"]):
             # non-dimensional parameter where 1e4 is to convert asize to micron
@@ -117,15 +127,18 @@ class Qabs_utils:
             # imaginary refractive index as complex number
             refrel = complex(data["real_m"][ii], data["im_m"][ii])
             # call bhmie that does the magic
-            S1, S2, qext, qsca, qbak, gsca = bhmie(xr, refrel, nang)
+            s1, s2, qext, qsca, qbak, gsca = bhmie(xr, refrel, nang)
             # store data
             data_q["qabs"].append(qext-qsca)
             data_q["qsca"].append(qsca)
             data_q["qbak"].append(qbak)
         # stores wavelengths
         data_q["wlen"] = data["wlen"]
+        # convert to Hz
+        data_q["freq"] = self.constants["clight"] / (data_q["wlen"] * 1e-4)
+
         # convert to numy arrays
-        self.data_q = {lab: np.array(x) for lab,x in data_q.iteritems()}
+        self.data_q = {lab: np.array(x) for lab, x in data_q.iteritems()}
 
     # ******************************
     def plot(self, xref="wlen", what=None, fname="output.png", ptype=plt.loglog):
@@ -134,6 +147,9 @@ class Qabs_utils:
 
         if what is None:
             what = ["qsca", "qabs"]
+
+        if type(ptype) is str:
+            ptype = eval("plt." + ptype)
 
         plt.clf()
         for wh in what:
@@ -148,11 +164,17 @@ class Qabs_utils:
 
             ptype(data_q[xref], data_q[wh], label=wh)
 
+        if xref == "wlen":
+            xlabel = "$\\lambda / \\mu$m"
+        elif xref == "freq":
+            xlabel = "Hz"
+        else:
+            sys.exit("ERROR: unknonw xref " + xref + " for the xlabel")
+
         # add some ornaments to plot
-        plt.xlabel("$\\lambda / \\mu$m")
+        plt.xlabel(xlabel)
         plt.ylabel(" or ".join(what))
         plt.legend(loc="best")
         plt.savefig(fname)
 
         print "Plot saved to " + fname
-
