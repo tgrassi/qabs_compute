d44ddccad25a6ad02d41ca1bbf14ae2549cf2854
tgrassi <tommasograssi@gmail.com>
2018-09-29 19:29:59 +0200

    PY: main test updated to show different features

--- test_01.py
+++ test_01.py
@@ -3,42 +3,65 @@ from utils import QabsManager
 # create object
 q = QabsManager()
 
-# load data from file with given format
-core = q.load_material("eps_Sil.dat", name="core")
+# load core data from file
+core = q.load_material("data/eps_Sil.dat")
 
-core.plot_ref_index("ref_index.png")
+# plot refractive index
+core.plot_ref_index("ref_index_core.png")
+
+# compute opacity for core material
+core.compute_kappa()
+
+# store core opacity to file
+core.save_kappa("kappa_core.out")
+
 
 # load data for mantle/coating material
-mantle = q.load_material("water_ice.dat", labs=["wlen", "real_m", "im_m"], name="mantle")
+mantle = q.load_material("data/water_ice.dat", labs=["wlen", "real_m", "im_m"])
 
-core.compute_kappa()
-core.save_kappa("test.dat")
+# create new composite material, core and mantle
+icy = q.make_optical([core, mantle])
 
-
-asdjlksd
 
 # write a report with some information on the loaded data
 q.report()
 
-# create new composite material
-icy = q.make_optical([core, mantle])
 
+# compute Qabs for a given size
 core.compute_q(1e-6)
 mantle.compute_q(1e-6)
+
+# plot Qabs
+core.plot_q("qabs_core.png")
+mantle.plot_q("qabs_mantle.png")
+
+
+# compute Qabs for the core+mantle system
 icy.compute_q([1e-6, 1.2e-6])
 
-icy.plot_q()
+# plot Qabs
+icy.plot_q("qabs_icy.png")
 
-icy.load_kappa("test.dat")
+# set layer thickness
+icy.dust.alayer = 1e-7
 
-# icy.dust.alayer = 1e-7
-# icy.compute_kappa()
-# icy.save_kappa("test.dat")
+# print some info on the dust used
+icy.dust.report()
 
-icy.plot_kappa()
-
-dadasdas
-
-icy.dust.alayer = 1e-6
+# compute opacity for the compound material
 icy.compute_kappa()
-icy.dust.add_plot_kappa("output.png")
+
+# store to file
+icy.save_kappa("kappa_icy.out")
+
+# plot opacity to file
+icy.plot_kappa("kappa_icy_1e7.png")
+
+# change layer thickness
+icy.dust.alayer = 1e-6
+
+# compute opacity
+icy.compute_kappa()
+
+# plot opacity to file
+icy.dust.add_plot_kappa("kappa_icy_1e6.png")
