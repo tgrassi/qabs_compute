60db92c2d69e7a27d0e9a5ec61a026fe8a17057a
tgrassi <tgrassi@nbi.dk>
2018-10-05 15:52:32 +0200

    PY: test04 OK

--- test_04.py
+++ test_04.py
@@ -33,24 +33,23 @@ opt.load_kappa("data/kappa_oss94_v45.dat")
 opt.add_plot_kappa("kappa.png", postfix=" (ref, ratio 4.5)", linestyle=":")
 
 core_silicate.compute_kappa()
-#core_silicate.add_plot_kappa("kappa.png", postfix=" (Si Oss93)")
+# core_silicate.add_plot_kappa("kappa.png", postfix=" (Si Oss93)")
 
 
 # load amorphous carbon, compute opacity. and plot
 core_carbon = q.load_material("data/eps_carb_P93.dat", ["wlen", "real_m", "im_m"])
 core_carbon.dust.rho_bulk = 2e0
 core_carbon.compute_kappa()
-#core_carbon.add_plot_kappa("kappa.png", postfix=" (aC P93)")
-# frac = np.array([1e1**(7.55-12.)*28., 1e1**(8.56-12.)*12.*0.2])
-frac = np.array([3.63e-5*28., 3.3e-4*12.*0.6])
+# core_carbon.add_plot_kappa("kappa.png", postfix=" (aC P93)")
 
-# frac = np.array([1e0, 0.6])
+frac = np.array((1., 0.475862068965517))
+frac /= sum(frac)
 
 norm = core_carbon.dust.mass_normalization * frac[1] \
        + core_silicate.dust.mass_normalization * frac[0]
 
 merged = q.merge_kappa([core_silicate, core_carbon], frac, mass_normalization=norm)
-merged.add_plot_kappa("kappa.png", postfix=" (merged, bare)",
+merged.add_plot_kappa("kappa.png", postfix=" (merged, ratio 0.0)",
                       linestyle="-")
 
 # make composite materials (Si+ice and aC+ice)
@@ -62,12 +61,10 @@ for vratio in [0.5, 4.5]:
     aratio = ((vratio + 1e0)**(1./3.) - 1e0)
     composite_silicate.dust.aratio = aratio
     composite_silicate.compute_kappa()
-    #composite_silicate.add_plot_kappa("kappa.png", postfix=" (Si+ice, ratio %.1f)" % vratio)
 
     composite_carbon.dust.aratio = aratio
     composite_carbon.dust.rho_bulk = 2e0
     composite_carbon.compute_kappa()
-    #composite_carbon.add_plot_kappa("kappa.png", postfix=" (aC+ice, ratio %.1f)" % vratio)
 
     merged = q.merge_kappa([composite_silicate, composite_carbon], frac, mass_normalization=norm)
     merged.add_plot_kappa("kappa.png", postfix=" (merged, ratio %.1f)" % vratio,
