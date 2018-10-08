e89edcd5d1a72593adedef482b3c1b90fb3a5b88
tgrassi <tommasograssi@gmail.com>
2018-10-08 18:26:59 +0200

    PY: test03 CO ice

--- test_03.py
+++ test_03.py
@@ -4,38 +4,46 @@ import numpy as np
 # create object
 q = QabsManager()
 
-# load data from file with given format
+# load core optical properties
+core_silicate = q.load_material("data/eps_Sil_Oss92.dat", ["wlen", "real_m", "im_m"])
+core_carbon = q.load_material("data/eps_carb_P93.dat", ["wlen", "real_m", "im_m"])
+
+# load mantle optical properties
 mantle = q.load_material("eps_CO.dat", labs=["wlen", "real_eps", "im_eps"])
 
-# load core optical properties
-core = q.load_material("data/eps_Sil.dat")
+# add mantle to cores
+composite_silicate = q.make_optical([core_silicate, mantle])
+composite_carbon = q.make_optical([core_carbon, mantle])
 
-# create new material
-composite = q.make_optical([core, mantle])
+core_silicate.compute_kappa()
+core_carbon.dust.rho_bulk = 2e0
+core_carbon.compute_kappa()
 
+# volume fractions
+frac = np.array((1., 0.475862068965517))
+frac /= sum(frac)
 
-# QABS
-# compute qabs and qsca for a given grain size and variable mantle radii
-asize_core = 1e-7  # cm
-# loop on mantle radius (note: radius not shell thickness)
-for asize_mantle in np.linspace(1.1e-7, 3.1e-7, 5):
-    print "mantle radius, cm:", asize_mantle
-    composite.compute_q([asize_core, asize_mantle])
-    # plot
-    composite.add_plot_q("composite_q.png", postfix=(" (%.1e cm)" % asize_mantle))
+merged = q.merge_kappa([core_silicate, core_carbon], frac)
+merged.add_plot_kappa("kappa.png", postfix=" (merged, ratio 0.0)",
+                      linestyle="-")
 
-
-# OPACITY
-q.clear_plots()
 # compute kappa for different shell thicknesses
-for alayer in np.logspace(-7., -5., 3):
-    print "tickness, cm:", alayer
-    composite.dust.alayer = alayer
-    composite.compute_kappa()
-    # plot
-    composite.add_plot_kappa("composite_kappa.png", postfix=(" (%.1e cm)" % alayer))
+for vratio in [0.5, 4.5]:
+    # size ratio from volume ratio
+    aratio = ((vratio + 1e0)**(1./3.) - 1e0)
 
+    # silicate + mantle opacity
+    composite_silicate.dust.aratio = aratio
+    composite_silicate.compute_kappa()
 
+    # carbon + mantle opacity
+    composite_carbon.dust.aratio = aratio
+    composite_carbon.dust.rho_bulk = 2e0  # g/cm3
+    composite_carbon.compute_kappa()
+
+    merged = q.merge_kappa([composite_silicate, composite_carbon], frac)
+    merged.add_plot_kappa("kappa.png", postfix=" (merged, ratio %.1f)" % vratio,
+                          xlim=(1e2, None), ylim=(None, 1e2))
 
 
 
