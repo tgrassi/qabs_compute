5d47ec3f4b4bb8d08d788100eed1de298c270941
tgrassi <tommasograssi@gmail.com>
2018-11-12 14:10:09 +0100

    PY: maxwell garnett

--- material.py
+++ material.py
@@ -114,6 +114,8 @@ class Material:
 
         # compute real_m and im_m from eps
         if "real_eps" in data and "im_eps" in data:
+            print "NOTE: real_m_computed computed from real_eps and im_eps"
+            print "NOTE: im_m_computed computed from real_eps and im_eps"
             # compute refractive index from eps
             e1 = data["real_eps"]
             e2 = data["im_eps"]
@@ -122,12 +124,12 @@ class Material:
 
         # compute im_m from eps if missing
         if "im_m" not in data:
-            print "NOTE: im_m computed from eps"
+            print "NOTE: im_m computed from im_eps_computed"
             data["im_m"] = data["im_m_computed"]
 
         # compute real_m from eps if missing
         if "real_m" not in data:
-            print "NOTE: real_m computed from eps"
+            print "NOTE: real_m computed from real_eps_computed"
             data["real_m"] = data["real_m_computed"]
 
         # compute im_m1 from im_m if missing
@@ -137,6 +139,8 @@ class Material:
 
         # compute eps if missing from im_m and real_m
         if "real_eps" not in data:
+            print "NOTE: real_eps computed from real_m and im_m"
+            print "NOTE: im_eps computed from real_m and im_m"
             data["real_eps"] = data["real_m"]**2 - data["im_m"]**2
             data["im_eps"] = 2e0 * data["real_m"] * data["im_m"]
 
@@ -153,77 +157,70 @@ class Material:
         self.data = data
 
     # ***********************
-    def extrapolate(self, wmax, nlast=30):
+    def extrapolate(self, wmax, nlast=70):
         import numpy as np
         import sys
         from scipy.optimize import curve_fit
         from lmfit import minimize, Parameters, Parameter
 
-        def ff(x, fl, wl2, gamma):
-            clight = 3e10
-            echarge = 4.80320425e-10
-            me = 9.10938e-28
-            pre = 2e0 * echarge**2 / me / clight
-            return fl * wl2 * x**2 / (2e0 * np.pi * clight * (x**2 - wl2)
-                                      + 1j * gamma * wl2 * x)
+        def ffw(x, c_wp, c_wo, c_gamma, c_off):
+            fun = c_off + c_wp**2 / (c_wo**2 - x**2 - 1j * x * c_gamma)
+            return fun
 
-        def residuals(paramz, x, data):
-            fl = paramz['fl'].value
-            wl2 = paramz['wl2'].value
-            gamma = paramz['gamma'].value
+        def residualsi(paramz, x, data):
+            c_wp = paramz['wp'].value
+            c_wo = paramz['wo'].value
+            c_gamma = paramz['gamma'].value
+            c_off = paramz['off'].value
 
-            diff = ff(x, fl, wl2, gamma) - data
-            # The only change required is to use view instead of abs.
-            return diff.view(np.float)
+            diff = ffw(x, c_wp, c_wo, c_gamma, c_off) - data
+
+            return diff.view(np.float32)
+
+        clight = 3e10
+        th = self.data["wlen"][-nlast:] * 1e-4
+        th = 2e0 * np.pi * clight / th
+        #ydata = (self.data["real_m"][-nlast:] + 1j * self.data["im_m"][-nlast:])**2 - 1e0
+        ydata = self.data["real_eps"][-nlast:] + 1j * self.data["im_eps"][-nlast:]
 
         params = Parameters()
-        params.add("fl", value=1e15, min=1e9, max=1e20)
-        params.add("wl2", value=1e-4, min=1e-10, max=1e-1)
-        params.add("gamma", value=1e14, min=1e8, max=1e20)
+        params.add("wp", value=1e1**13.47, min=1e1**13.35, max=1e1**13.55)
+        params.add("wo", value=1e1**13.58, min=1e1**13.5, max=1e1**13.7)
+        params.add("gamma", value=1e1**13.32, min=1e1**13.25, max=1e1**13.4)
+        params.add("off", value=2.48, min=2.2, max=2.6)
 
-        th = self.data["wlen"][-nlast:] * 1e-4
-        ydata = (self.data["real_m"][-nlast:] + 1j * self.data["im_m"][-nlast:])**2 - 1e0
+        result = minimize(residualsi, params, args=(th, ydata), method="leastsq", nan_policy='omit')
+        wp = 1e1**13.35 #result.params["wp"].value
+        wo = 1e1**13.51 # result.params["wo"].value
+        gamma = 1e1**12.92 #result.params["gamma"].value
+        off = 2.52 #result.params["off"].value
 
-        result = minimize(residuals, params, args=(th, ydata), method="leastsq")
-        fl = result.params["fl"].value
-        wl2 = result.params["wl2"].value
-        gamma = result.params["gamma"].value
+        for k in result.params.keys():
+            print result.params[k]
+            print np.log10(result.params[k].value)
 
-        print result.params
-
-        zdata = ff(th, fl, wl2, gamma)
+        zdata = ffw(th, wp, wo, gamma, off)
 
         import matplotlib.pyplot as plt
         plt.clf()
-        plt.plot(th, ydata)
-        plt.plot(th, zdata)
+        plt.plot(th, ydata.real)
+        plt.plot(th, zdata.real)
         plt.show()
 
-        askljdlkas
-
-        def fe1(x2, wp2, wo2, gamma):
-            return 1e0 + wp2 * (wo2 - x2) / ((wo2 - x2)**2 + gamma**2 * x2)
-
-        def fe2(x, wp2, wo2, gamma):
-            return wp2 * gamma * x / ((wo2 - x**2)**2 + gamma**2 * x**2)
-
-        step = 1
-        c1 = c2 = None
-        s1 = s2 = None
-        for _ in range(10):
-            c2, s2 = curve_fit(fe2, 1e0 / self.data["wlen"][-nlast::step],
-                               self.data["im_eps"][-nlast::step], p0=c1)
-            c1, s1 = curve_fit(fe1, 1e0 / self.data["wlen"][-nlast::step]**2,
-                               self.data["real_eps"][-nlast::step], p0=c2)
-        if np.inf in s1 or np.inf in s2:
-            sys.exit("ERROR: infinity in covariance matrix!")
+        plt.clf()
+        plt.plot(th, ydata.imag)
+        plt.plot(th, zdata.imag)
+        plt.show()
 
         wmax_data = self.data["wlen"][-1]
         for wlen in np.linspace(wmax_data, wmax, 20):
-            e1 = fe1(1e0 / wlen**2, c1[0], c1[1], c1[2])
-            e2 = fe2(1e0 / wlen, c1[0], c1[1], c1[2])
-            m2 = ((-e1 + np.sqrt(e1 ** 2 + e2 ** 2)) / 2.) ** 0.5
-            m1 = e2 / 2. / m2
+            ee = ffw(wlen * 1e-4, wp, wo, gamma, off)
+            e1 = np.real(ee)
+            e2 = 0e0
+
+            cconj = np.sqrt(e1 ** 2 + e2 ** 2)
+            m1 = np.sqrt((cconj + e1) / 2e0)
+            m2 = np.sqrt((cconj - e1) / 2e0)
 
             self.data["wlen"] = np.append(self.data["wlen"], wlen)
             self.data["real_m"] = np.append(self.data["real_m"], m1)
@@ -242,3 +239,34 @@ class Material:
             row = [str(self.data[k][ii]) for k in ["wlen", "real_eps", "im_eps", "real_m", "im_m"]]
             fout.write(" ".join(row) + "\n")
         fout.close()
+
+    # ***********************
+    # add an spherical impurities with given volume filling factor.
+    # optical properties of the impurities are taken from material_impurity
+    def add_impurity(self, material_impurity, volume_filling_factor):
+        from scipy.interpolate import interp1d
+        from numpy import sqrt
+
+        # interpolate real and imaginary dielectric functions
+        feps1 = interp1d(material_impurity.data["wlen"], material_impurity.data["real_eps"])
+        feps2 = interp1d(material_impurity.data["wlen"], material_impurity.data["im_eps"])
+
+        # alias name for the volume filling factor
+        vff = volume_filling_factor
+        for ii, wlen in enumerate(self.data["wlen"]):
+            # complex impurities material dielectric
+            eps = complex(feps1(wlen), feps2(wlen))
+            # complex matrix material dielectric
+            eps_m = complex(self.data["real_eps"][ii], self.data["im_eps"][ii])
+
+            # Bruggemann rule, see Bohren+Huffmann 1983, eq. 8.50
+            g = vff * (eps - eps_m) / (eps + 2e0 * eps_m)
+            eps_av = eps_m * (1e0 + 3e0 * g / (1e0 - g))
+
+            # set new dielectric
+            self.data["real_eps"][ii] = eps1 = eps_av.real
+            self.data["im_eps"][ii] = eps2 = eps_av.imag
+            # compute refractive index from dielectric
+            cconj = sqrt(eps1**2 + eps2**2)
+            self.data["real_m"][ii] = sqrt((cconj + eps1) / 2e0)
+            self.data["im_m"][ii] = sqrt((cconj - eps1) / 2e0)
